a9de513ef7cd9dfd9639f3433f13bea1592418f8
Gina Frankel <g.frankel22@gmail.com>
2020-07-31 15:11:13 +0100

    Return promise from products api

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,18 @@
+import React from "react";
+import axios from "axios";
+
 function Home() {
+  //interacting with the outside world - something where we don't know what the result will be
+  React.useEffect(() => {
+    getProducts();
+  }, []);
+
+  //async automatically returns promoise
+  async function getProducts() {
+    const url = "http://localhost:3000/api/products";
+    const response = await axios.get(url);
+    console.log(response.data);
+  }
   return <>home</>;
 }
 
