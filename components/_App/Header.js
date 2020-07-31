9e3bbcad4c62e480aa5f93cc16fb92f426f6f340
Gina Frankel <g.frankel22@gmail.com>
2020-07-31 10:59:20 +0100

    Render an Image as a Link

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -1,9 +1,20 @@
-import { Menu, Container } from "semantic-ui-react";
+import { Menu, Container, Image } from "semantic-ui-react";
+import Link from "next/link";
 
 function Header() {
   return (
     <Menu fluid={true} id="menu" inverted={true}>
-      <Container text></Container>
+      <Container text>
+        <Link href="/">
+          <Menu.Item header>
+            <Image
+              size="mini"
+              src="/static/logo.svg"
+              style={{ marginRight: "1em" }}
+            />
+          </Menu.Item>
+        </Link>
+      </Container>
     </Menu>
   );
 }
