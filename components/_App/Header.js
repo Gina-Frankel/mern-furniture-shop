7e8a4d60f473176d95af249e2f5e9709d7a8b14d
Gina Frankel <g.frankel22@gmail.com>
2020-07-31 10:50:59 +0100

    Render Menu + Container component from semantic-ui-react

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -1,5 +1,11 @@
+import { Menu, Container } from "semantic-ui-react";
+
 function Header() {
-  return <>Header</>;
+  return (
+    <Menu fluid={true} id="menu" inverted={true}>
+      <Container text></Container>
+    </Menu>
+  );
 }
 
 export default Header;
