2622b876a59c8e91bd240aed27648f6a9a647d88
Gina Frankel <g.frankel22@gmail.com>
2020-08-01 11:07:14 +0100

    Get products from api products

--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,28 @@ import axios from "axios";
 import ProductList from "../components/Index/ProductList";
 
 function Home(props) {
-  console.log(props);
+  //can also destructure {products} from props then print products
+  console.log(props.products);
   //interacting with the outside world - something where we don't know what the result will be
-  React.useEffect(() => {
-    getProducts();
-  }, []);
+  // React.useEffect(() => {
+  //   getProducts();
+  // }, []);
 
   //async automatically returns promoise
-  async function getProducts() {
-    const url = "http://localhost:3000/api/products";
-    const response = await axios.get(url);
-    console.log(response.data);
-  }
+  // async function getProducts() {
+  //   console.log(response.data);
+  // }
   return <>home</>;
 }
 
 //method on component
 // get initial data that will be added t props of this component
 // need to connect in _app page
-Home.getInitialProps = () => {
+Home.getInitialProps = async () => {
+  const url = "http://localhost:3000/api/products";
+  const response = await axios.get(url);
+  return { products: response.data };
+
   //fetch data on server
   //make get request
   //return response data as an object as props is an object
