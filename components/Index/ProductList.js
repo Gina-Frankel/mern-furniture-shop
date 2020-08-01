bbb14a4e7efdab07399bd6cc303b7a397533a99d
Gina Frankel <g.frankel22@gmail.com>
2020-08-01 11:48:20 +0100

    Render a Card Group with items mapped from props

--- components/Index/ProductList.js
+++ components/Index/ProductList.js
@@ -1,5 +1,23 @@
-function ProductList() {
-  return <>ProductList</>;
+import { Card } from "semantic-ui-react";
+
+function ProductList(props) {
+  function mapProductsToItem(products) {
+    return products.map((product) => ({
+      header: product.name,
+      image: product.mediaUrl,
+      meta: `$${product.price}`,
+      color: "pink",
+      fluid: true,
+      childKey: product._id,
+      // Query string
+      href: `/product?_id${product._id}`,
+    }));
+  }
+  return (
+    <>
+      <Card.Group items={mapProductsToItem(props.products)} />
+    </>
+  );
 }
 
 export default ProductList;
