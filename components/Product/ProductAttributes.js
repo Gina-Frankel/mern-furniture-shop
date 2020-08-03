a054c882c1fad7b265840ea0de778f39bc360239
Gina Frankel <g.frankel22@gmail.com>
2020-08-03 11:48:00 +0100

    Render Header + description

--- components/Product/ProductAttributes.js
+++ components/Product/ProductAttributes.js
@@ -1,5 +1,12 @@
-function ProductAttributes() {
-  return <>ProductAttributes</>;
+import { Header } from "semantic-ui-react";
+
+function ProductAttributes({ description }) {
+  return (
+    <>
+      <Header as="h3"> About this Product </Header>
+      <p>{description}</p>
+    </>
+  );
 }
 
 export default ProductAttributes;
