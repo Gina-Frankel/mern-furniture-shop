4bf5a7c011a3bb94aea77b9d8bbbac42d9cc5d56
Gina Frankel <g.frankel22@gmail.com>
2020-08-04 17:02:43 +0100

    Render Input Component so user can add product to cart

--- components/Product/AddProductToCart.js
+++ components/Product/AddProductToCart.js
@@ -1,5 +1,20 @@
+import { Input } from "semantic-ui-react";
+
 function AddProductToCart() {
-  return <>AddProductToCart</>;
+  return (
+    <Input
+      type="number"
+      min="1"
+      placeholder="Quantity"
+      // default value
+      value={1}
+      action={{
+        color: "orange",
+        content: "Add to Cart",
+        icon: "plus cart",
+      }}
+    ></Input>
+  );
 }
 
 export default AddProductToCart;
