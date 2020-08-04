a4d72ceb837407f83e33db54b09b8c578d2174ca
Gina Frankel <g.frankel22@gmail.com>
2020-08-04 16:45:24 +0100

    Create function to toggle modal + render

--- components/Product/ProductAttributes.js
+++ components/Product/ProductAttributes.js
@@ -1,7 +1,13 @@
+// to use hook
+import React from "react";
+
 import { Header, Button, Modal } from "semantic-ui-react";
 import { Model, modelNames } from "mongoose";
 
 function ProductAttributes({ description }) {
+  // Hook to use state
+  const [modal, setModal] = React.useState(false);
+  console.log(modal);
   return (
     <>
       <Header as="h3"> About this Product </Header>
@@ -10,14 +16,15 @@ function ProductAttributes({ description }) {
         icon="trash alternate outline"
         color="red"
         content="Delete Product"
+        onClick={() => setModal(true)}
       />
-      <Modal open={true} dimmer="blurring">
+      <Modal open={modal} dimmer="blurring">
         <Modal.Header>Confirm delete</Modal.Header>
         <Modal.Content>
           <p>Are you sure you want to delete this product?</p>
         </Modal.Content>
         <Modal.Actions>
-          <Button content="cancel" />
+          <Button content="cancel" onClick={() => setModal(false)} />
           <Button
             negative
             icon="trash"
